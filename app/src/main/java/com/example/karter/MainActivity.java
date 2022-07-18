735f61308475048f2b79446b8fdafbabd2e1a44f
Abhi <abhishekvelekar79200@gmail.com>
2022-07-18 19:30:35 +0530

    Login Page layout added

--- app/src/main/java/com/example/karter/MainActivity.java
+++ app/src/main/java/com/example/karter/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AllProducts.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
 
