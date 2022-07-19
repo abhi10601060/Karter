321da64bc478af23b189a9fcc5665e585606cb1c
Abhi <abhishekvelekar79200@gmail.com>
2022-07-19 12:00:36 +0530

    Sign-up page Ui layout Added

--- app/src/main/java/com/example/karter/LoginActivity.java
+++ app/src/main/java/com/example/karter/LoginActivity.java
@@ -2,13 +2,34 @@ package com.example.karter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
+    private FloatingActionButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void initViews() {
+        login_btn=findViewById(R.id.login_floating_btn);
     }
 }
