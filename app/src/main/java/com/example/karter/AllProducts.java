700f66c32fa313f9e2394b2fdd111ce2fc664f9d
Abhi <abhishekvelekar79200@gmail.com>
2022-07-20 17:58:21 +0530

    Profile icon and name added in AllProduct toolabar menu

--- app/src/main/java/com/example/karter/AllProducts.java
+++ app/src/main/java/com/example/karter/AllProducts.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ActionMenuItemView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -13,12 +15,17 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AllProducts extends AppCompatActivity {
 
@@ -28,6 +35,9 @@ public class AllProducts extends AppCompatActivity {
     private DrawerLayout drawer ;
     private MaterialToolbar toolbar;
     private NavigationView navigationView;
+    private TextView profile_name;
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
 
 
     @Override
@@ -39,6 +49,8 @@ public class AllProducts extends AppCompatActivity {
         Utils.initSharedPreferences(this);
 
         setSupportActionBar(toolbar);
+
+        handleToolbarProfile();
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.drawer_close,
                 R.string.drawer_open);
@@ -101,14 +113,49 @@ public class AllProducts extends AppCompatActivity {
         transaction.replace(R.id.container,fragmentAllProducts);
         transaction.commit();
 
+        handleAuth();
 
 
 
+    }
+
+    private void handleToolbarProfile() {
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.profile_menu_item:
+                        Toast.makeText(AllProducts.this, "profile selected", Toast.LENGTH_SHORT).show();
+                        // TODO: 20-07-2022 move to profile Activity 
+                        break;
+                    default:
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void handleAuth() {
+        FirebaseUser user = auth.getCurrentUser();
+        String[]name = user.getDisplayName().split(" ");
+        profile_name.setText("hi..!\n"+name[0]);
     }
 
     private  void  initViews(){
         drawer=findViewById(R.id.drawer);
         toolbar=findViewById(R.id.toolbar);
         navigationView=findViewById(R.id.navigation_view);
+        profile_name=findViewById(R.id.txt_profile_name);
     }
 }
